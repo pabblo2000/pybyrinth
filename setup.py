121a59c3ec8dfe91057e709983a15e43d0feb69f
pablo <palvaroh2000@gmail.com>
2024-10-09 00:37:52 +0200

    libreria subida

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
-from setuptools import setup, find_packages
+# setup.py
+
+from setuptools import setup
 
 setup(
-    name="pyyrinth",
-    version="1.1.0",
-    packages=find_packages(),
-    install_requires=[],  
-    author="Pablo Álvaro Hidalgo",
-    author_email="palvaroh2000@gmail.com",
-    description="",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/tu_usuario/tu_proyecto",
+    name='pybyrinth',  
+    version='1.1.0',  
+    description="A Python library for creating, solving, and visualizing mazes using Depth-First Search (DFS) and Breadth-First Search (BFS) algorithms.",
+    long_description=open('README.md', encoding='utf-8').read(),  
+    long_description_content_type='text/markdown',  
+    author='Pablo Álvaro Hidalgo',  
+    author_email='palvaroh2000@gmail.com',  
+    license='MIT',  
+    py_modules=['pybyrinth'], 
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent', 
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6',  
+    keywords='maze, labyrinth, pathfinding, search, DFS, BFS', 
+    url='https://github.com/pabblo2000/pynacci',  
 )
+
+# Para subir la librería a PyPI, ejecuta el siguiente comando en la terminal:
+# python setup.py sdist 
+
+# Luego, ejecuta el siguiente comando en la terminal:
+# twine upload --repository pynacci dist/*
